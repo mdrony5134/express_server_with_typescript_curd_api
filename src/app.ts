29f6aebad6e088b44f3b33feb60465f25ff0a1e6
mdrony5134 <rony830388@gmail.com>
2026-02-02 23:07:40 +0600

    using express routing to make crud api

--- src/app.ts
+++ src/app.ts
@@ -3,18 +3,15 @@ import express, {
   type Request,
   type Response,
 } from "express";
+import { productsRouter } from "./app/products.route.js";
 const app: Application = express();
+app.use(express.json());
+
+
+app.use("/products", productsRouter);
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello girls sfs!!!");
-});
-
-app.get("/todos", (req: Request, res: Response) => {
-  res.send("Hello bangladesh!");
-});
-
-app.post("/todos/create-todo", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.json({ message: "Welcome to the products API" });
 });
 
 export default app;
---
+++ src/app/products.route.ts
@@ -0,0 +1,93 @@
+import express, { application, type Request, type Response } from 'express';
+import fs from 'fs';
+import path from 'path';
+
+
+export const productsRouter = express.Router();
+const pathName = path.join(process.cwd(), 'db', 'data.json');
+
+
+
+// Create a new product
+
+productsRouter.post('/create-product', (req: Request, res: Response) => {
+  const { name, price, category, inStock } = req.body; 
+console.log(req.body)
+    const data = fs.readFileSync(pathName, "utf-8");
+    const parsedData = JSON.parse(data);
+    const newProduct = {
+      id: parsedData.products.length + 1,
+      name, 
+        price,
+        category,
+        inStock
+    };
+    parsedData.products.push(newProduct);
+    fs.writeFileSync(pathName, JSON.stringify(parsedData, null, 2));
+    res.json({ message: "Product created successfully", product: newProduct });
+});
+
+// get all products
+productsRouter.get('/', (req: Request, res: Response) => {
+ const data = fs.readFileSync(pathName, "utf-8");
+ console.log("data", data)
+ const parsedData = JSON.parse(data);
+    res.json({message: "Products retrieved successfully", products: parsedData.products });
+});
+
+// get single product
+productsRouter.get('/single-product/:id', (req: Request, res: Response) => {
+  const { id } = req.params;
+  const data = fs.readFileSync(pathName, "utf-8");
+    const parsedData = JSON.parse(data);
+    const product = parsedData.products.find((product: any) => product.id === parseInt(id as string));
+    if (product) {
+      res.json({ message: "Product retrieved successfully", product }); 
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+});
+
+// delete a product
+productsRouter.delete('/delete-product/:id', (req: Request, res: Response) => {
+  const { id } = req.params;
+  const data = fs.readFileSync(pathName, "utf-8");  
+    const parsedData = JSON.parse(data);
+     if(parsedData.products.length>0){
+        const updatedProducts = parsedData.products.filter((product: any) => product.id !== parseInt(id as string));
+        parsedData.products = updatedProducts;
+        fs.writeFileSync(pathName,
+            JSON.stringify(parsedData, null, 2)
+        );
+        res.json({ message: "Product deleted successfully" });
+     }  else{
+        res.status(404).json({ message: "Product not found" });
+     }
+});
+
+// update a product
+productsRouter.put('/update-product/:id', (req: Request, res: Response) => {
+  const { id } = req.params;
+    const { name, price, category, inStock } = req.body;
+    const data = fs.readFileSync(pathName, "utf-8");
+    const parsedData = JSON.parse(data);
+    const productIndex = parsedData.products.findIndex((product: any) => product.id === parseInt(id as string));
+    if (productIndex !== -1) {
+      parsedData.products[productIndex] = {
+        id: parseInt(id as string),
+        name,
+        price,
+        category,
+        inStock
+        };
+        fs.writeFileSync(pathName, JSON.stringify(parsedData, null, 2));
+        res.json({ message: "Product updated successfully", product: parsedData.products[productIndex] });
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+});
+
+
+
+
+
